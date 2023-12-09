d157d972b7dcede036b503077c8ddb2606d17c79
Goran Alković <goranalkovic@users.noreply.github.com>
2023-12-09 01:48:15 +0100

    design tweaks

--- src/App.svelte
+++ src/App.svelte
@@ -4,8 +4,8 @@
   import AudioPlayer from './AudioPlayer.svelte';
 
   async function loadList() {
-    let response = await fetch('list.json');
-    return response.json();
+	let response = await fetch('list.json');
+	return response.json();
   }
 
   let searchTerm = '';
@@ -14,41 +14,48 @@
 
 <main>
   <div class="left">
-    <h1>
-      <img src="zoka-cyan.png" alt="Zorica Marković" width="64" />
-      <br />
-      Zorica Marković soundboard
-    </h1>
-    <input
-      class="search"
-      type="search"
-      bind:value={searchTerm}
-      placeholder="Traži, ne pitaj"
-    />
+	<h1>
+	  <img src="zoka-cyan.png" alt="Zorica Marković" width="64" />
+	  <span>
+		Zorica
+	  </span>
+	  <span>
+		Marković
+	  </span>
+	  <span>
+		soundboard
+	  </span>
+	</h1>
+	<input
+	  class="search"
+	  type="search"
+	  bind:value={searchTerm}
+	  placeholder="Traži, ne pitaj"
+	/>
   </div>
 
   {#await loadList()}
-    <Spinner />
+	<Spinner />
   {:then files}
-    {@const filteredFiles = files.filter(({ label }) => label.toLowerCase().includes(searchTerm.toLowerCase()))}
-    <div class="grid">
-      {#each filteredFiles as file, i (i)}
-        <div transition:slide|global>
-          <AudioPlayer {file} />
-        </div>
-      {/each}
+	{@const filteredFiles = files.filter(({ label }) => label.toLowerCase().includes(searchTerm.toLowerCase()))}
+	<div class="grid">
+	  {#each filteredFiles as file, i (i)}
+		<div transition:slide|global>
+		  <AudioPlayer {file} />
+		</div>
+	  {/each}
 
-      {#if filteredFiles?.length < 1}
-      <div class="search-empty">
-        <img src="/zoka-yellow.png" alt="Zorica te gleda mrko" width="64">
-        <h2>Šta me gledaš, a? <br /> Misliš da tu ima išta?</h2>
-      <p>Traži nešto drugo!</p>
-      </div>
-    {/if}
-    </div>
+	  {#if filteredFiles?.length < 1}
+	  <div class="search-empty">
+		<img src="/zoka-yellow.png" alt="Zorica te gleda mrko" width="64">
+		<h2>Šta me gledaš, a? <br /> Misliš da tu ima išta?</h2>
+	  <p>Traži nešto drugo!</p>
+	  </div>
+	{/if}
+	</div>
 
-    
+
   {:catch}
-    <p>Greška kod učitavanja</p>
+	<p>Greška kod učitavanja</p>
   {/await}
 </main>
